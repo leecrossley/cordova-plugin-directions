67d2946fe1375ec1e4277c07fb75a804de6818ce
Lee Crossley <leee@hotmail.co.uk>
2014-02-24 10:36:01 +0000

    Add wp8 placeholder

---
+++ src/wp8/Directions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Net;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace Cordova.Extension.Commands
+{
+    public class Directions : BaseCommand
+    {
+    }
+}
