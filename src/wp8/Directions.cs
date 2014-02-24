137656287766680993bc39b989c64a374ff62405
Lee Crossley <leee@hotmail.co.uk>
2014-02-24 10:55:51 +0000

    Implement wp8

--- src/wp8/Directions.cs
+++ src/wp8/Directions.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Net;
-using System.Windows;
-using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+using System.Device.Location;
+using System.Runtime.Serialization;
 using WPCordovaClassLib.Cordova;
 using WPCordovaClassLib.Cordova.Commands;
 using WPCordovaClassLib.Cordova.JSON;
@@ -10,5 +9,30 @@ namespace Cordova.Extension.Commands
 {
     public class Directions : BaseCommand
     {
+        [DataContract]
+        public class Location
+        {
+            [DataMember(Name = "latitude")]
+            public double Latitude;
+
+            [DataMember(Name = "longitude")]
+            public double Longitude;
+        }
+
+        public void navigateTo(string options)
+        {
+            var args = JsonHelper.Deserialize<string[]>(options);
+            var location = JsonHelper.Deserialize<Location>(args[0]);
+
+            var directions = new BingMapsDirectionsTask
+            {
+                End = new LabeledMapLocation
+                {
+                    Location = new GeoCoordinate(location.Latitude, location.Longitude)
+                }
+            };
+            directions.Show();
+            DispatchCommandResult(new PluginResult(PluginResult.Status.OK));
+        }
     }
 }
