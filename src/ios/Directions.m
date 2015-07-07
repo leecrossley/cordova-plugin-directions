f2702944756ddf6b405e4ad1c2d38557f345faf2
Lee Crossley <leee@hotmail.co.uk>
2015-07-07 09:17:13 +0100

    Replace deprecated success:callbackId (iOS) #5

--- src/ios/Directions.m
+++ src/ios/Directions.m
@@ -4,12 +4,10 @@
 
 - (void)navigateTo:(CDVInvokedUrlCommand*)command
 {
-    
-    NSString* callbackId = [command callbackId];
     NSString* address = [[[command arguments] objectAtIndex:0] valueForKey:@"address"];
     NSString* lat = [[[command arguments] objectAtIndex:0] valueForKey:@"latitude"];
     NSString* lng = [[[command arguments] objectAtIndex:0] valueForKey:@"longitude"];
-    
+
     NSString* url;
     if (address != nil) {
         address = [address stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
@@ -18,12 +16,12 @@
         url = [NSString stringWithFormat:@"http://maps.apple.com/maps?daddr=%@, %@", lat, lng];
     }
     [[UIApplication sharedApplication] openURL: [NSURL URLWithString: url]];
-    
+
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsString:url];
-    
-    [self success:result callbackId:callbackId];
+
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
 @end
